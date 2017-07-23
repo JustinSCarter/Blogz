25c8a5ca6878ef806f8e377a28f2ca39169a33c5
Justin Carter <jscarter@umich.edu>
2017-07-22 23:08:27 -0700

    So many things have changed!

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, render_template, flash, redirect
+from flask import Flask, request, render_template, flash, redirect,session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:Final@localhost:8889/blogz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://Blogz:Final@localhost:8889/Blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = '6ZEnHDF5MJ'
@@ -15,7 +15,7 @@ class Blog(db.Model):
     body = db.Column(db.String(500))
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
         self.owner = owner
@@ -25,41 +25,41 @@ class Blog(db.Model):
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True)
+    username = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
     blogs = db.relationship('Blog', backref='owner')
 
-    def __init__(self, email, password):
-        self.email = email
+    def __init__(self, username, password):
+        self.username = username
         self.password = password
 
     def __repr__(self):
-        return '<User %r>' % self.email
+        return '<User %r>' % self.Username
 
 @app.route('/blog', methods=['POST', 'GET'])
-def index():
-    '''displays blog posts on a home page'''
+def blog_list():
     id = request.args.get('id')
     if id:
         blog = Blog.query.filter_by(id=id).first()
-        return render_template('blog.html', title=blog.title, body=blog.body)
-    blogs = Blog.query.order_by(Blog.id.desc()).all()
-    return render_template('blog.html', title='Build a Blog', blogs=blogs)
+        return render_template('SingleUser.html', title=blog.title, body=blog.body)
+    blogs = Blog.query.order_by(Blog.id.asc()).all()
+    return render_template('blog.html', title='List of all blog posts:', blogs=blogs)
 
 @app.route('/newpost', methods=['POST', 'GET'])
-def blog_post():
+def new_blog():
     '''Form to create a new blog entry'''
     #add the author of a post to the stored data
     if request.method == 'POST':
         new_post_title = request.form['title']
         new_post_body = request.form['body']
+        author = User.query.filter_by(username=session['username']).first()
         if not new_post_body or not new_post_title:
             if not new_post_title:
                 flash('A title is required.', 'titleerror')
             if not new_post_body:
                 flash('Your post needs some content.', 'bodyerror')
             return render_template('newpost.html', body=new_post_body, title=new_post_title)
-        new_post = Blog(new_post_title, new_post_body)
+        new_post = Blog(new_post_title, new_post_body, author)
         db.session.add(new_post)
         db.session.commit()
         id = new_post.id
@@ -68,16 +68,16 @@ def blog_post():
 
     return render_template('newpost.html')
 
-@app.route("/signup", methods=['GET', 'POST'])
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        email = request.form['email']
+        username = request.form['username']
         password = request.form['password']
         verify = request.form['verify']
-        existing_user = User.query.filter_by(email=email).first()
+        existing_user = User.query.filter_by(username=username).first()
 
-        if not is_email(email):
-            flash('Uh-oh! "' + email + '" does not seem like an email address.')
+        if not username:
+            flash('You need a username.')
             return redirect('/register')
 
         if password != verify:
@@ -89,40 +89,26 @@ def register():
             return redirect('/register')
 
         else:
-            new_user = User(email, password)
+            new_user = User(username, password)
             db.session.add(new_user)
             db.session.commit()
-            session['email'] = email
+            session['username'] = username
             return redirect('/')
 
     else:
         return render_template('register.html')
 
-def is_email(string):
-    # for our purposes, an email string has an '@' followed by a '.'
-    # there is an embedded language called 'regular expression' that would crunch this implementation down
-    # to a one-liner, but we'll keep it simple:
-    atsign_index = string.find('@')
-    atsign_present = atsign_index >= 0
-    if not atsign_present:
-        return False
-    else:
-        domain_dot_index = string.find('.', atsign_index)
-        domain_dot_present = domain_dot_index >= 0
-        return domain_dot_present
-
-
 @app.route("/login", methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        email = request.form['email']
+        username = request.form['username']
         password = request.form['password']
-        user = User.query.filter_by(email = email).first()
+        user = User.query.filter_by(username = username).first()
 
         if user and user.password == password:
-            session['email'] = email
+            session['username'] = username
             flash("Logged in!")
-            return redirect('/')
+            return redirect('/newpost')
         elif not user:
             flash("That user doesn't appear to exist")
             return redirect('/login')
@@ -134,21 +120,23 @@ def login():
 
 @app.route('/index')
 def index():
+    users = User.query.order_by(User.id.desc()).all()
+    return render_template('index.html', users=users)
 
 @app.route("/logout")
 def logout():
-    del session['email']
-    return redirect("/"):
+    del session['username']
+    return redirect("/")
 
 @app.route('/')
 def gotoindex():
-    '''redirects to /blog route'''
-    return redirect("/blog", code=302)
+    '''redirects to /index route'''
+    return redirect("/index", code=302)
 
 @app.before_request
 def require_login():
-    allowed_routes = ['login', 'register']
-    if request.endpoint not in allowed_routes and 'email' not in session:
+    allowed_routes = ['login', 'register', 'blog', 'index']
+    if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login')
 
 app.secret_key = 'M/N_dHktEbgAvL8uqf8Ta3jfZhDvGsYDaK9'
